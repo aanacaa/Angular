da529b9967104e6b3f87b543aeae4d42de251dad
Ana Andrade <anacrln.andrade@gmail.com>
2020-09-07 22:40:41 -0300

    Commit

---
+++ TypeScript_Alura/01-alurabank/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,38 @@
+
+class NegociacaoController {
+
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+    //private _negociacoes: Negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    constructor() {
+
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+    }
+
+    adiciona(event: Event) {
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+        this._negociacoes.adiciona(negociacao);
+/*
+        this._negociacoes.paraArray().length = 0;//pode zerar o array
+
+        this._negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor)
+        })
+*/
+        console.log(negociacao);
+    }
+}
